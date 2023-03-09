aea932565d6b99e6a6c33771b1259148dc64ff91
Jean king <jeandedieu405@gmail.com>
2023-03-09 11:24:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from simulation import Simulation
 
-def write_to_file():
-    file = open("log.text", "w")
-    file.write("simulation")
-    file.close()
-write_to_file()
 
 process = Simulation()
 process.run_simulation( 10)
