f79f18b35cb55aff8ffd24cc619661df3e351140
Jean king <jeandedieu405@gmail.com>
2023-03-09 11:28:59 -0500

    Update pasta.py

--- pasta.py
+++ pasta.py
@@ -1,7 +1,10 @@
 from order import Order
 
 class Pasta(Order):
-    def __init__(self):
+    def __init__(self, dish_name, price):
         super().__init__()
-        self.dish_name = "pasta"
-        self.price = 5
+        self.dish_name = dish_name
+        self.price = price
+    
+    def create_order(self):
+        return f"{self.dish_name} pasta ordered for {self.price}$"
