12c1553e11a1334e3f20754c33ffa75e7f52799f
Jean king <jeandedieu405@gmail.com>
2023-03-09 11:30:18 -0500

    Update salad.py

--- salad.py
+++ salad.py
@@ -3,5 +3,5 @@ from order import Order
 class Salad(Order):
     def __init__(self):
         super().__init__()
-        self.dish_name = "salad"
-        self.price = 20
+        self.dish_name = dish_name
+        self.price = price
