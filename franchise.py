cebef86c170369df31dbd6796cbdf1088e5d67a4
Jean king <jeandedieu405@gmail.com>
2023-03-09 11:24:43 -0500

    Update franchise.py

--- franchise.py
+++ franchise.py
@@ -1,10 +1,27 @@
 from orderfactory import OrderFactory
+from order import Order
+from logger import Logger
 
 class Franchise:
 
-    def location_number(self) -> int :
-        self.location_number = 1
+    def __init__(self, location_number):
+        self.location_number = location_number
+        self.logger = Logger()
+        self.orderfactory = OrderFactory()
 
-    def place_order(self):
-        oder_factory = OrderFactory()
-        order = oder_factory.create_order("pizza")
+    def location_number(self, dish_name) :
+        if dish_name == "pizza":
+           return 1
+        if dish_name == "pasta":
+           return 2
+        if dish_name == "salad":
+           return 3
+
+    def place_order(self, franchise_name, dish_name, price):
+        self.orderfactory = OrderFactory()
+        self.order = self.orderfactory.create_order(dish_name, price)
+
+        self.order = self.orderfactory.create_order(dish_name, price)
+        welcome_message = f"Welcome to TB Restaurant, location #{self.location_number}"
+        print(welcome_message)
+        self.logger.log_transaction( franchise_name, self.order.dish_name, price)
